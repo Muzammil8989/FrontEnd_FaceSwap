61c57305913f66affb4bcf8dd910b3bfa5eb30fd
Muhammad Muzammil Khan <94923293+Muzammil8989@users.noreply.github.com>
2024-12-27 12:43:29 -0800

    Update celebrity.ts

--- src/lib/data/celebrity.ts
+++ src/lib/data/celebrity.ts
@@ -7,8 +7,11 @@ export interface Celebrity {
   export const celebrities: Celebrity[] = [
     { id: 1, name: "Template 1", image: "https://static.rock.so/file/xpg30EKp/JA6Q2D5K/41843d0d4cdcee726fc34a64583d702f/HeyFusion_2024_11_06_10_37_28_2184385GenerativeImage.jpg" },
     { id: 2, name: "Template 2", image: "https://static.rock.so/file/xpg30EKp/JA6Q2D5K/fb89d92bad3be207520b0b36a6478ad2/HeyFusion_2024_11_06_10_20_00_1409940GenerativeImage.jpg" },
-    { id: 3, name: "Template 3", image: "https://www.freep.com/gcdn/-mm-/e0d90cdbba7b0ec079b6abb7354fe290d3edda2d/c=0-480-2160-3360/local/-/media/2017/02/11/DetroitFreePress/DetroitFreePress/636224442280884273-Dorian-Thompson-03.jpg" },
-    { id: 4, name: "Template 4", image: "https://i.ndtvimg.com/i/2017-07/lynn-aronberg-donald-trump-selfie-facebook_650x400_61501468602.jpg" },
-    { id: 5, name: "Template 5", image: "https://www.freep.com/gcdn/-mm-/f5f10dc281d1490b59383629aea4468cf9fe487d/c=0-0-1120-1494/local/-/media/2017/02/11/DetroitFreePress/DetroitFreePress/636224442271836099-Dorian-Thompson-05.jpg" }
+    { id: 3, name: "Template 3", image: "https://static.rock.so/file/xpg30EKp/JA6Q2D5K/53fe5b2f42450e4280f7d8f8fc7b5824/HeyFusion_2024_11_06_10_23_37_3529952GenerativeImage.jpg" },
+    { id: 4, name: "Template 4", image: "https://static.rock.so/file/xpg30EKp/JA6Q2D5K/931d80b02609d8503fe66b4a90c860bd/HeyFusion_2024_11_06_10_48_29_2601523GenerativeImage.jpg" },
+    { id: 5, name: "Template 5", image: "https://static.rock.so/file/xpg30EKp/JA6Q2D5K/caa6210c1fe6a1a028d022e6a2359757/HeyFusion_2024_11_06_10_03_44_5882187GenerativeImage.jpg" },
+       { id: 6, name: "Template 6", image: "https://static.rock.so/file/xpg30EKp/JA6Q2D5K/b6d51fc4ca1a761390c6ab123a4e4443/HeyFusion_2024_11_06_10_08_14_1490539GenerativeImage.jpg" },
+       { id: 7, name: "Template 7", image: "https://static.rock.so/file/xpg30EKp/JA6Q2D5K/242a84f85999ac3657411f2832d7fec5/HeyFusion_2024_11_06_10_37_02_3664405GenerativeImage.jpg" },
+       { id: 8, name: "Template 8", image: "https://static.rock.so/file/xpg30EKp/JA6Q2D5K/b5388edbe4d5af851bd22edc93710aec/HeyFusion_2024_11_06_10_38_10_2755375GenerativeImage.jpg" }
   ];
   
