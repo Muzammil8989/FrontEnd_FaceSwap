ecd4a4fc1f72644837e44d2a27ee64893f86aa56
Muhammad Muzammil Khan <94923293+Muzammil8989@users.noreply.github.com>
2024-12-27 09:52:48 -0800

    Update page.tsx

--- src/app/celebrities/page.tsx
+++ src/app/celebrities/page.tsx
@@ -140,7 +140,7 @@ export default function Celebrities() {
       const formData = new FormData();
       formData.append("swapImage", swapFile);
 
-      const uploadResponse = await fetch("http://localhost:5000/uploadSwap", {
+      const uploadResponse = await fetch("https://face-swap-backend-gamma.vercel.app/uploadSwap", {
         method: "POST",
         body: formData,
       });
@@ -296,34 +296,9 @@ export default function Celebrities() {
         body: JSON.stringify({ imageUrl: finalUrl }),
       });
 
-<<<<<<< HEAD
       if (!createRes.ok) {
         const createErrData = await createRes.json();
         throw new Error(createErrData.error || "Error creating Printful tasks");
-=======
-      console.log(await res.json());
-
-      // if (!res.ok) {
-      //   const errData = await res.json();
-      //   throw new Error(errData.error || "Error creating mockups");
-      // }
-      // const data: MockupResponse = await response.json();
-      const data = await res.json();
-      if (res.ok) {
-        // const { mockupUrls } = data;
-        // Convert mockup response into a local array
-        // const generated = mockupUrls.map((mockup) => ({
-        //   id: mockup.productId,
-        //   name: mockup.productName,
-        //   image: mockup.mockupImageUrl,
-        // }));
-        console.log("Mockups generated:", data);
-        
-        setMockups(data.mockups);
-        toast.success("Mockups generated successfully!");
-      } else {
-        toast.error(data.message || "Error generating mockups.");
->>>>>>> 5518f0600bfca1d32bc5c053df172d76be28536c
       }
 
       const createData = await createRes.json();
