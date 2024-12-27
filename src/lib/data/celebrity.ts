a99063a566e507ed58274e9add21bb1d8cd7bafc
Muhammad Muzammil Khan <94923293+Muzammil8989@users.noreply.github.com>
2024-12-27 12:35:51 -0800

    Update celebrity.ts

--- src/lib/data/celebrity.ts
+++ src/lib/data/celebrity.ts
@@ -5,7 +5,7 @@ export interface Celebrity {
   }
   
   export const celebrities: Celebrity[] = [
-    { id: 1, name: "Template 1", image: "https://i.imgur.com/PUfNbwC.jpeg" },
+    { id: 1, name: "Template 1", image: "https://static.rock.so/file/xpg30EKp/JA6Q2D5K/41843d0d4cdcee726fc34a64583d702f/HeyFusion_2024_11_06_10_37_28_2184385GenerativeImage.jpg" },
     { id: 2, name: "Template 2", image: "https://media.npr.org/assets/img/2015/07/09/maggietrump_custom-05c2c19944e3f1ecceac0ca25b08f72e042a9108.jpg" },
     { id: 3, name: "Template 3", image: "https://www.freep.com/gcdn/-mm-/e0d90cdbba7b0ec079b6abb7354fe290d3edda2d/c=0-480-2160-3360/local/-/media/2017/02/11/DetroitFreePress/DetroitFreePress/636224442280884273-Dorian-Thompson-03.jpg" },
     { id: 4, name: "Template 4", image: "https://i.ndtvimg.com/i/2017-07/lynn-aronberg-donald-trump-selfie-facebook_650x400_61501468602.jpg" },
